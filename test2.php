422862a94b38871bbb9ef7f3253837c08d2067de
schnauzer <Schnauzer@gmail.com>
2023-12-25 00:29:55 +0800

    12/24 just wrote it casually

--- test2.php
+++ test2.php
@@ -9,7 +9,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <title></title>
     </head>
         <script>
-            document.getElement
+            // document.getElement
         </script>
     <body>
     <img src="kuaikuai.jpg" width="100px" height="100px" style="float:left" title="這是張防Bug乖乖圖" alt="這是張防Bug乖乖圖" >
@@ -35,15 +35,23 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <input type="submit" id="previous1" name="previous1" value="上一頁">
 
         <input type="submit" id="next1" name="next1" value="下一頁"  >
-        <input type="submit" id="order1" name="order1" value="排序" />
-        <br>
+        <input type="submit" id="order_highest" name="order_highest" value="價格:高到低" />
+        <input type="submit" id="order_lowest" name="order_lowest" value="價格:低到高" />
+        
+        <br>       
     <!-- <input type="text" id="i1" name="i11" value=""> -->
+
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------- -->
     <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "chocolate_db";
 
+
+    $n_per_page = 10;
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -53,7 +61,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     }
     echo "Connected successfully <br>";
 
-    $sql ="SELECT * FROM chocolate LIMIT 10";
+    $conn->query("SET NAMES 'UTF8'");
+
+    $sql ="SELECT * FROM chocolate LIMIT ".$n_per_page;
 
     //第一次載入時初始化start和end變數, 之後則是GET 表單submit之後的值
     if (empty($_GET['start_number']) && empty($_GET['end_number']) ){
@@ -66,7 +76,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         echo "接收到START & END<br>";
      }
 
-    //每一頁顯示筆數預設為1-------------------------------------------------------------------------------------
+    //每一頁顯示筆數-預設為1-------------------------------------------------------------------------------------
     if (isset($_GET['submit1'])){
         $end_number = $start_number + $_GET['n_per_page'] - 1;
         $sql = "SELECT * FROM chocolate Where Data_orderid BETWEEN ".$start_number." and ".$end_number;
@@ -79,27 +89,30 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 
     //下一頁和上一頁(10個商品)---------------------------------------------------------------
     if (isset($_GET['next1'])){
-        $start_number += 10;
-        $end_number += 10 ;
+        $start_number += $n_per_page;
+        $end_number += $n_per_page ;
         // echo $start_number.$end_number;
         $sql = "SELECT * FROM chocolate WHERE Data_orderid BETWEEN ".$start_number." and ".$end_number;
     
     }elseif(isset($_GET['previous1'])){
         
-        $start_number -= 10;
-        $end_number -= 10 ;
+        $start_number -= $n_per_page;
+        $end_number -= $n_per_page ;
         //起點為負值 則初始化--------------------------------------------------------------------- 
-        if ($start_number < 10 ){
-            $start_number =0;
+        if ($start_number < 0 ){
+            $start_number =1;
             $end_number = 10;
         }
         
         $sql = "SELECT * FROM chocolate WHERE Data_orderid BETWEEN ".$start_number." and ".$end_number;
     }
     //排序-----------------------------------------------------------------------------------
-    if (isset($_GET['order1'])){
-        $sql = "SELECT * FROM chocolate ORDER BY Data_price DESC";
+    if (isset($_GET['order_highest'])){
+        $sql = "SELECT * FROM chocolate ORDER BY Data_price DESC limit 10";
+    }else if (isset($_GET['order_lowest'])){
+        $sql = "SELECT * FROM chocolate ORDER BY Data_price ASC limit 10";
     };
+    ;
     //---------------------------------------------------------------------------------------
     $result = $conn->query($sql);
     echo "<table border='5px'> ";
@@ -112,7 +125,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
             echo "<tr><th>".$row['Data_orderid']."</th>";
             echo "<td><img src=.\chocolate_images\\".$row['Data_pid'].".jpg alt='沒有圖片' width='100px'></td>";
             echo "<td>".$row['Data_name']."</td><td>"."\$".$row['Data_price']."</td>";
-            echo "<td><input type='number' id='quantity' name='quantity' value='0' min='0' max='99'></td>";
+            //echo "<td><input type='number' id='quantity' name='quantity' value='0' min='0' max='99'></td>";
             // echo "<td>&nbsp;&nbsp;&nbsp;0</td></tr>";
             
         }
@@ -120,13 +133,66 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     echo "0 results";
     }
     echo "</table>";
+
+    $result->close();
     $conn->close();
+    
 
  ?>
 
-        <input type="hidden" name="start_number"  value="<?php echo $start_number; ?>">
-        <input type="hidden"  name="end_number"  value="<?php echo $end_number; ?>">
+        <input type="text" name="start_number"  value="<?php echo $start_number; ?>">
+        <input type="text"  name="end_number"  value="<?php echo $end_number; ?>">
     </form>
+
+
+    <?php  
+    function search($text){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "chocolate_db";
+    
+  
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        //if (isset($_GET['search1']) && isset($_GET['search_text']))  {
+            $sql = "SELECT * FROM chocolate WHERE Data_name LIKE '%".$text."%' Limit 10";
+        //};
+
+        $result = $conn->query($sql);
+        echo "<table border='5px'> ";
+        if ($result->num_rows > 0) {
+        // output data of each row
+            echo "<tr><th>編號</th><th>圖片</th><th>品名</th><th>價格</th><th>購買數量</th><tr>";
+            while($row = $result->fetch_assoc()) {
+                // echo  $row["Data_name"].  $row["Data_price"]."<br>";
+                
+                echo "<tr><th>".$row['Data_orderid']."</th>";
+                echo "<td><img src=.\chocolate_images\\".$row['Data_pid'].".jpg alt='沒有圖片' width='100px'></td>";
+                echo "<td>".$row['Data_name']."</td><td>"."\$".$row['Data_price']."</td>";
+                //echo "<td><input type='number' id='quantity' name='quantity' value='0' min='0' max='99'></td>";
+                // echo "<td>&nbsp;&nbsp;&nbsp;0</td></tr>";
+                
+            }
+
+        $result->close();
+        $conn->close();
+
+    
+        }
+    }
+
+    
+
+    //search('義美');
+    ?>
+    <?php
+
+    ?>
     <!-- <script>
         function showUser(str) {
             var xmlhttp = new XMLHttpRequest();
